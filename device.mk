409f6e361595c7f641839cc0a5388623e21b2de2
Shashank Sivakumar <quic_shassiva@quicinc.com>
2025-05-11 13:27:44 +0800

    sweet: Use core 64 bit only makefile
    Moving the corresponding core_64_bit_only make file after removing the
    entry from the common64.mk file. This is to enable the GSI components for
    64 bit option available.
    
    Change-Id: Ieaf709e602312783c5a0f1ac898c4bedf8b21197
    Signed-off-by: Shashank Sivakumar <quic_shassiva@quicinc.com>
    Signed-off-by: eidawon <projecteidawon@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
